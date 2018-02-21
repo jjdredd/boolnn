e7f300925bd7ed19ad7ae1d021a2b70b7232bfd7
jjdredd <the.guard@mail.ru>
2018-02-21 14:52:23 -0700

    needfix started layer dword

--- network/layers.cpp
+++ network/layers.cpp
@@ -8,9 +8,8 @@
 // LayerBIT class
 // 
 LayerBIT::LayerBIT(unsigned N_in, unsigned N_out)
-	: N_in(N_in), N_out(N_out), W(N_in, 2 * N_out) {
+	: N_in(N_in), N_out(N_out), W(N_in, 2*N_out), B(2*N_out) {
 
-	B.reserve(2 * N_out);
 	N_bits = N_in * N_out + 2 * N_out;
 }
 
@@ -31,6 +30,35 @@ std::vector<bool> LayerBIT::Compute(std::vector<bool> input) const {
 
 void LayerBIT::FlipBit(unsigned N) {
 	
+	unsigned n = N < GetNDOF() ? N : N%GetNDOF();
+	unsigned k = n - W.GetNDOF();
+
+	if (k > 0) {
+		B[k] = !B[k];
+	} else {
+		unsigned i = n / W.N;
+		unsigned j = n % W.N;
+		W[i][j] = !W[i][j];
+	}
+}
+
+
+
+//
+// LayerDWORD
+// 
+LayerDWORD::LayerDWORD(unsigned N_in, unsigned N_out)
+	: N_in(N_in), N_out(N_out), W(N_in, 2*N_out), B(2*N_out) {
+	
+	N_bits = N_in * N_out + 2*N_out;
+}
+
+unsigned LayerDWORD::GetNDOF() {
+	return N_bits;
+}
+
+void LayerDWORD::FlipBit(unsigned N) {
+
 	unsigned n = N < GetNDOF() ? N : N%GetNDOF();
 	unsigned k = n - W.GetNDOF();
 
