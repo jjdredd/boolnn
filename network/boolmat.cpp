fc32a4d7418a345246fe80fd6b60f12efb70a036
jjdredd <the.guard@mail.ru>
2018-02-20 22:46:32 -0700

    finished BoolMat

--- network/boolmat.cpp
+++ network/boolmat.cpp
@@ -71,19 +71,58 @@ BoolMat BoolMat::operator& (const BoolMat rhs) const {
 	BoolMat res(rhs.M, rhs.N);
 
 	if (N != rhs.M) {
-		std::cerr << "size mismatch in BoolMat::operator&"
+		std::cerr << "size mismatch in BoolMat::operator&(const BoolMat)"
 			  << std::endl;
 		return res;
 	}
 	
-	for (unsigned i = 0; i < N; i++) {
+	for (unsigned i = 0; i < M; i++) {
+		for (unsigned j = 0; j < rhs.N; j++) {
+			for (unsigned k = 0; k < N; k++) {
+				res.W[i][j] = W[i][k] & rhs.W[k][j];
+			}
+		}
 	}
+
 	return res;
 }
 
-std::vector<bool> BoolMat::operator& (const std::vector<bool>) const {
+std::vector<bool> BoolMat::operator& (const std::vector<bool> rhs) const {
 
 	std::vector<bool> res;
+
+	if (N != rhs.size()) {
+		std::cerr << "size mismatch in"
+			  << " BoolMat::operator& (const std::vector<bool>"
+			  << std::endl;
+		return res;
+	}
+
+	res.reserve(rhs.size());
+
+	for (unsigned i = 0; i < M; i++) {
+		res[i] = W[i] & rhs;
+	}
+
+	return res;
+}
+
+BoolMat BoolMat::operator^ (const BoolMat rhs) const {
+
+	BoolMat res(M, N);
+
+	if (rhs.M != M || rhs.N != N) {
+		std::cerr << "size mismatch in"
+			  << "BoolMat BoolMat::operator^ (const BoolMat)"
+			  << std::endl;
+		return res;
+	}
+
+	for (unsigned i = 0; i < M; i++) {
+		for (unsigned j = 0; j < N; j++) {
+			res.W[i][j] = W[i][j] ^ rhs.W[i][j];
+		}
+	}
 
 	return res;
 }
